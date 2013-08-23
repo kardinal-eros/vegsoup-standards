56ccbcb19348e4998fd338d337539fc6d078dda7
Roland Kaiser <kardinal.eros@gmail.com>
2013-08-23 23:25:47 +0200

    deleted lock file

--- austrian standard list 2008/TestForUniqueAbbr.R
+++ austrian standard list 2008/TestForUniqueAbbr.R
@@ -1,7 +1,7 @@
 #	run from shell
 #	R -f TestForUniqueAbbr.R 
 
-df <- read.csv2("austrian standard list 2008.csv", stringsAsFactors = FALSE)
+df <- read.csv2("~/Documents/vegsoup-standards/austrian standard list 2008/austrian standard list 2008.csv", stringsAsFactors = FALSE)
 res <- df$abbr[duplicated(df$abbr)]
 
 print(res)
