f67126b7534f4c313ded1c4e091fb0cc24704736
Roland Kaiser <kardinal.eros@gmail.com>
2021-01-30 15:42:24 +0100

    corrected typo

--- austrian standard list 2008/TestForUniqueAbbr.R
+++ austrian standard list 2008/TestForUniqueAbbr.R
@@ -3,7 +3,7 @@
 
 x <- read.csv2("~/Documents/vegsoup-standards/austrian standard list 2008/austrian standard list 2008.csv", stringsAsFactors = FALSE)
 
-test <- df$abbr[ duplicated(x$abbr) ]
+test <- x$abbr[ duplicated(x$abbr) ]
 
 print(test)
 
