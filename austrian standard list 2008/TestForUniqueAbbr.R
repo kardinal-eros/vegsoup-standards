8b26623d6134c1b9f3a1ccb38ef3c4ceb0a89c28
Roland Kaiser <kardinal.eros@gmail.com>
2021-01-30 12:26:02 +0100

    deleted 2 duplicated taxa

--- austrian standard list 2008/TestForUniqueAbbr.R
+++ austrian standard list 2008/TestForUniqueAbbr.R
@@ -1,7 +1,12 @@
 #	run from shell
 #	R -f TestForUniqueAbbr.R 
 
-df <- read.csv2("~/Documents/vegsoup-standards/austrian standard list 2008/austrian standard list 2008.csv", stringsAsFactors = FALSE)
-res <- df$abbr[duplicated(df$abbr)]
+x <- read.csv2("~/Documents/vegsoup-standards/austrian standard list 2008/austrian standard list 2008.csv", stringsAsFactors = FALSE)
 
-print(res)
+test <- df$abbr[ duplicated(x$abbr) ]
+
+print(test)
+
+test <- x$taxon[ duplicated(x$taxon) ]
+
+print(test)
